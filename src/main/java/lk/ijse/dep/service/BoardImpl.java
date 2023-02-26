299bec13893942c45998b768aaf391ccf5a3b22a
mipunaruwan <nipunakalpa1234@gmail.com>
2023-02-26 20:23:25 +0530

    encapsulationed Winner class

--- src/main/java/lk/ijse/dep/service/BoardImpl.java
+++ src/main/java/lk/ijse/dep/service/BoardImpl.java
@@ -1,6 +1,6 @@
 package lk.ijse.dep.service;
 
-public class BoardImpl {
+public class BoardImpl implements Board{
     private static final int NUM_OF_COLS = 6;
     private static final int NUM_OF_ROWS = 5;
     private final BoardUI boardUI;
@@ -12,4 +12,33 @@ public class BoardImpl {
         this.pieces = new Piece[NUM_OF_ROWS][NUM_OF_COLS];
     }
 
+    @Override
+    public BoardUI getBoardUI() {
+        return null;
+    }
+
+    @Override
+    public int findNextAvailableSpot(int col) {
+        return 0;
+    }
+
+    @Override
+    public boolean isLegalMove(int col) {
+        return false;
+    }
+
+    @Override
+    public boolean existLegalMoves() {
+        return false;
+    }
+
+    @Override
+    public void updateMove(int col, Piece move) {
+
+    }
+
+    @Override
+    public Winner findWinner() {
+        return null;
+    }
 }
